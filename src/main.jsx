5115ed593ec6a61e1e1ed0168a20001d229ef34b
ProgramerG <116060169+afnanahmadtariq@users.noreply.github.com>
2024-09-26 13:59:53 +0500

    fixing hehe

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
