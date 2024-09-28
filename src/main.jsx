9f75fb074ed9f5638f99b9493e22b32bbbf27232
ProgramerG <116060169+afnanahmadtariq@users.noreply.github.com>
2024-09-28 20:12:20 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-  <SpeedInsights />
+    <App />,
+    <SpeedInsights />
+  </StrictMode>
 )
