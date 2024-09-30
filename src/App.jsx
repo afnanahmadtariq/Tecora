1abf5b916b27a138abfd087bc96d8a46210c5e68
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2024-09-30 06:15:28 +0500

    Setting up things....

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import tecoraLogo from './assets/Tecora logo.svg'
 import './App.css'
 
 function App() {
@@ -9,24 +8,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <img src={tecoraLogo} className="logo" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Tecora</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Click me {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Coming Soon, Please be Patient!<br></br>
+        You can click the above in the mean time.
       </p>
     </>
   )
