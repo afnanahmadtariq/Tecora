2506662080410ed59c4095c93906592ac9d51467
Coding Syed <148377982+aligee12@users.noreply.github.com>
2024-12-24 22:50:13 +0500

    trying to adjust project card

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,7 @@ import { FiExternalLink } from "react-icons/fi";
 import { fetchProjects } from "../api/projects";
 import { useNavigate } from "react-router-dom"; // Import useNavigate for navigation
 import htmlImg from "../assets/html.jpeg";
+import ProjectCard from "../components/projectCard/ProjectCard";
 
 const tabs = ["All", "Following", "Not Following"];
 
@@ -61,29 +62,30 @@ export default function Projects() {
         ))}
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {filteredProjects.map((project) => (
-          <div
-            key={project.id}
-            className="bg-white dark:bg-gray-800 rounded-lg shadow-sm transition-colors duration-200"
-            onClick={() => navigate(`/projects/${project.id}`)} // Use navigate to route to the project details page
-          >
-            <div className="project-img-section w-full">
-              <img src={htmlImg} alt="" className="projectImage w-full" />
-            </div>
-            <div className="project-desc py-4 px-3 flex flex-col justify-center">
-              <h3 className="project-title text-lg font-medium text-blue-600 dark:text-blue-400">
-                {project.title}
-              </h3>
-              <p className="project-desc text-sm text-gray-600 dark:text-gray-300">
-                {project.description}
-              </p>
-              <button className="flex items-center justify-center py-2 text-blue-500 dark:text-blue-400 hover:text-blue-600 dark:hover:text-blue-300">
-                Open Project
-                <FiExternalLink className="ml-1" />
-              </button>
-            </div>
-          </div>
+          <ProjectCard project={project} />
+          // <div
+          //   key={project.id}
+          //   className="bg-white dark:bg-gray-800 rounded-lg shadow-sm transition-colors duration-200"
+          //   onClick={() => navigate(`/projects/${project.id}`)} // Use navigate to route to the project details page
+          // >
+          //   <div className="project-img-section w-full">
+          //     <img src={htmlImg} alt="" className="projectImage w-full" />
+          //   </div>
+          //   <div className="project-desc py-4 px-3 flex flex-col justify-center">
+          //     <h3 className="project-title text-lg font-medium text-blue-600 dark:text-blue-400">
+          //       {project.title}
+          //     </h3>
+          //     <p className="project-desc text-sm text-gray-600 dark:text-gray-300">
+          //       {project.description}
+          //     </p>
+          //     <button className="flex items-center justify-center py-2 text-blue-500 dark:text-blue-400 hover:text-blue-600 dark:hover:text-blue-300">
+          //       Open Project
+          //       <FiExternalLink className="ml-1" />
+          //     </button>
+          //   </div>
+          // </div>
         ))}
       </div>
     </div>
